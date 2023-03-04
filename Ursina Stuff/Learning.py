cd78db0d821326d54149ed79ff2f7ea0e44e9c55
Rubin006 <rubinmorina@gmail.com>
2023-03-04 22:42:02 +0000

    UPDATE

---
+++ Ursina Stuff/Learning.py
@@ -0,0 +1,17 @@
+from ursina import *
+
+app = Ursina(size=(1600,1200))
+
+cube = Entity(model="cube",color = color.orange,scale=(2,2,2),texture="brick")
+
+
+
+def update():
+    if held_keys["w"]: cube.rotation_x += time.dt * 250
+    if held_keys["s"]: cube.rotation_x -= time.dt * 250
+
+window.borderless = False
+window.exit_button.visible = False
+window.borderless = False
+EditorCamera()
+app.run()

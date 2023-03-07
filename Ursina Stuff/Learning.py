9383bfdd9f4d22dc83aac0245723c3df2ce76094
Rubin-006 <122726254+Rubin-006@users.noreply.github.com>
2023-03-07 08:21:20 +0000

    UPDATE

--- Ursina Stuff/Learning.py
+++ Ursina Stuff/Learning.py
@@ -7,8 +7,10 @@ cube = Entity(model="cube",color = color.orange,scale=(2,2,2),texture="brick")
 
 
 def update():
-    if held_keys["w"]: cube.rotation_x += time.dt * 250
-    if held_keys["s"]: cube.rotation_x -= time.dt * 250
+    if held_keys["d"]: cube.x += time.dt * 5
+    if held_keys["a"]: cube.x -= time.dt * 5
+    if held_keys["w"]: cube.y += time.dt * 5
+    if held_keys["s"]: cube.y -= time.dt * 5
 
 window.borderless = False
 window.exit_button.visible = False

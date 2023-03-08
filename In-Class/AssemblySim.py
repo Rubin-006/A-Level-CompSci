c1dc8ff1175469cbb466e7f85346e49586f15e29
Rubin006 <rubinmorina@gmail.com>
2023-03-08 22:07:06 +0000

    UPDATE

--- In-Class/AssemblySim.py
+++ In-Class/AssemblySim.py
@@ -9,10 +9,23 @@ InstructionSet = {
     "0110":"EOR",
     "0111":"STR",
     "1000":"LDR",
-    "1001":"HALT"
+    "1001":"MOV",
+    "1010":"HALT"
 }
 
 
 registers = {f"R{i}":0 for i in range(15)}
 
 memory = {i:0 for i in range(15)}
+
+with open(r"In-Class\assemblyCode.txt") as asm:
+    r = asm.readlines()
+    for line in r:
+        line = line.split()
+        if InstructionSet[line[0]] == "MOV":
+            registers[line[1]] = float(line[2])
+
+        if InstructionSet[line[0]] == "ADD":
+            val = registers[line[2]] + registers[line[3]]
+            registers[line[1]] = val
+            print(val)

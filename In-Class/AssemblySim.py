789590221b0e673a69fc9c66f30fee555ae755cc
Rubin-006 <122726254+Rubin-006@users.noreply.github.com>
2023-03-09 15:15:35 +0000

    Update

--- In-Class/AssemblySim.py
+++ In-Class/AssemblySim.py
@@ -4,25 +4,59 @@ InstructionSet = {
     "ADD":"0001",
     "LSL":"0010",
     "LSR":"0011",
-    "AND":"0100",
-    "ORR":"0101",
-    "EOR":"0110",
-    "STR":"0111",
+    "BGT":"0100",
+    "BLT":"0101",
+    "BEQ":"0110",
+    "BNE":"0111",
     "LDR":"1000",
     "MOV":"1001",
-    "HALT":"1010"
+    "CMP":"1010",
+    "HALT":"1011"
 }
 registers = {f"R{i}":0 for i in range(15)}
 memory = {i:0 for i in range(15)}
 
+add = lambda a,b: a+b
+sub = lambda a,b: a-b
+bgt = lambda a,b: a>b
+blt = lambda a,b: a<b
+beq = lambda a,b: a==b
+bne = lambda a,b: a!=b
+
+
+
+
 with open(r"In-Class\assemblyCode.txt") as asm:
     r = asm.readlines()
+    branch = None
     for line in r:
         line = line.split()
-        if InstructionSet[line[0]] == "1001":
-            registers[line[1]] = float(line[2])
+        try:
+            match InstructionSet[line[0]]:
+                case "1001": # MOV
+                    registers[line[1]] = float(line[2])
+                case "0001": # ADD
+                    num = add(registers[line[2]],registers[line[3]])
+                    registers[line[1]] = num
+                case "1010": # CMP
+                    nums = [registers[line[1]],registers[line[2]]]
+                case "0100": # BGT
+                    cmp_res = bgt(nums[0],nums[1])
+                    if cmp_res: branch = f"{line[1]}:"
+                case "0101": # BLT
+                    cmp_res = blt(nums[0],nums[1])
+                    if cmp_res: branch = f"{line[1]}:"
+                case "0110": # BEQ
+                    cmp_res = beq(nums[0],nums[1])
+                    if cmp_res: branch = f"{line[1]}:"
+                case "0111": # BNE
+                    cmp_res = bne(nums[0],nums[1])
+                    if cmp_res: branch = f"{line[1]}:"
+                case "1011":
+                    break
+        except KeyError:
+            pass
+            
 
-        if InstructionSet[line[0]] == "0001":
-            val = registers[line[2]] + registers[line[3]]
-            registers[line[1]] = val
-            print(val)
+print(registers)
+                

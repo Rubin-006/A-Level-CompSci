f2480ba334937dbb2d6a70c1a10d8bf4415f9e06
Rubin-006 <122726254+Rubin-006@users.noreply.github.com>
2023-03-10 10:51:46 +0000

    UPDATE

--- Ursina Stuff/Learning.py
+++ Ursina Stuff/Learning.py
@@ -9,27 +9,21 @@ from ursina import *
 if __name__ == "__main__":
     app = Ursina(size=(1000,800))
 
-    sun = Entity(model="sphere",texture="sun.jpg")
-    earth = Entity(model="sphere",position = (10,0,0),texture="earth.jpg")
+    sun = Entity(model="sphere",texture="sun.jpg",scale=1)
+    earth = Entity(model="sphere",scale=.5,texture="earth.jpg")
 
+    t = -pi
     def update():
-        velocity = Vec3(30,0,0)
-        earth.x += (velocity.x)*0.001
-        velocity.x += 27*0.001
-        velocity.y += 27*0.001
-        earth.look_at(sun)
-        if held_keys["d"]: sun.x += time.dt * 5
-        if held_keys["a"]: sun.x -= time.dt * 5
-        if held_keys["w"]: sun.y += time.dt * 5
-        if held_keys["s"]: sun.y -= time.dt * 5
+        global t
+        t += 0.01
+        angle = pi*2
 
-
-
-
-
-
-
-
+        radius = 5
+        pd = 4
+        earth.x = -cos(pd*t + angle) * radius
+        earth.y = sin(pi/2-(pd*t + angle))* radius
+        earth.z = sin(pi-(pd*t + angle)) * radius
+        sun.rotation_y += 20*time.dt
 
     window.borderless = False
     window.exit_button.visible = False

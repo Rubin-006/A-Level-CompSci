fd1eea86fea0319f39fa0d76844d765abd3abebd
Rubin-006 <122726254+Rubin-006@users.noreply.github.com>
2023-03-20 11:11:13 +0000

    Update

--- In-Class/Haskell/Learning.hs
+++ In-Class/Haskell/Learning.hs
@@ -2,30 +2,17 @@ import Data.List
 main :: IO()
 
 fac :: Int -> Int
-fac n = aux n 1 -- Factorial Function
+fac n = aux n 1
  where
     aux n acc
      | n <= 1 = acc
      | otherwise = aux (n-1) (n*acc)
 
-
-
-f :: [Int] -> [Int]
-let counter = 0
-
-f (x:lst) =
-    if count % 2 == 0
-        then x = x
-            let count = count + 1
-            head lst
-            f lst
-        head lst
-        f lst
                      
 
 
 
 main = do
-    let lst = [1,2,3,4,5]
-    let result = f lst
+    val1 <- readLn
+    let result = fac val1
     print result

84c7aa55ae6f52b45cbd1071adb6a9c57dc93a3a
Rubin-006 <122726254+Rubin-006@users.noreply.github.com>
2023-03-30 10:29:47 +0100

    UPDATE

--- In-Class/Python/FDEcycle.py
+++ In-Class/Python/FDEcycle.py
@@ -2,17 +2,18 @@
 class Computer:
 
     def __init__(self):
-        self.RAM = {f"{i:08b}":"00000000" for i in range(256)}
+        self.RAM = {f"{i:08b}":"000 00000" for i in range(10)}
+        self.addresses = list(self.RAM.keys())
         self.registers = {
-            "PC":None,
-            "SR":None,
-            "CIR":None,
-            "MAR":None,
-            "MDR":None,
-            "ACC":None,
-            "GPR":None,
-            "GPR":None,
-            "GPR":None
+            "PC": self.addresses[0],
+            "SR": None,
+            "CIR": None,
+            "MAR": None,
+            "MDR": None,
+            "ACC": None,
+            "GPR": None,
+            "GPR": None,
+            "GPR": None
         }
         self.opcode = {
             "000": "store",
@@ -25,23 +26,50 @@ class Computer:
             "111": "NOT"
         }
 
-    def ALU(self):
+    def fetch(self):
+        data = self.registers["PC"]
+        self.registers["MAR"] = self.registers["PC"]
+        self.registers["MDR"] = self.RAM[self.registers["MAR"]]
+        self.registers["PC"] = self.addresses[self.addresses.index(data) + 1]
+        self.registers["CIR"] = self.registers["MDR"]
+        print("PC:", self.registers["PC"])
+        print("MAR:", self.registers["MAR"])
+        print("MDR:", self.registers["MDR"])
+        print("CIR:", self.registers["CIR"])
+    
+    def decode(self):
+        instruction = self.opcode[self.registers["MDR"][:3]]
+        data = self.registers["MDR"][3:]
+        return (instruction,data)
 
-        pass
+    def execute(self):
+        instruction, data = self.decode()
+        match instruction:
+            case "store":
+                print("Stored value magically")
+            case "load":
+                print("Loaded Value Magically")
+            case "add":
+                data1 = int((((self.RAM[bin(int(self.registers["MAR"],2) - 1)]))[3:]),2)
+                data2 = int(data,2)
+                print(data1+data2)
 
-
-
-def fetch():
-    pass
-
-def decode():
-    pass
-
-def execute():
-    pass
+    def cycle(self):
+        while True:
+            try:
+                self.fetch()
+                self.execute()
+            except:
+                break
 
 def main():
     comp = Computer()
+    comp.RAM["00000000"] = "00000001"
+    comp.RAM["00000001"] = "01000001"
+    comp.fetch()
+    comp.execute()
+    comp.fetch()
+    comp.execute()
 
 if __name__ == "__main__":
     main()

54e87136f93b8833e63bf888c4c4b244d0f677b1
Rubin006 <rubinmorina@gmail.com>
2023-04-23 00:18:20 +0100

    Update main.py

--- TK/main.py
+++ TK/main.py
@@ -3,18 +3,26 @@ import customtkinter as ctk
 
 ctk.set_appearance_mode("Dark")
 
-ctk.set_default_color_theme("green")    
+ctk.set_default_color_theme("green")
 
 
 root = ctk.CTk()
 root.geometry("800x600")
 root.title("Fun Testing")
 
-
-def press():
-    print("Dont press me")
         
-button = ctk.CTkButton(master = root, text = "Press Me",command=press)
-button.place(relx=0.5,rely=0.5, anchor = ctk.CENTER)
+button = ctk.CTkButton(
+    master = root,
+    text = "Press Me",
+    command= lambda: print("Don't Press me"),
+    fg_color="#E80",
+    hover_color="#E50"
+    )
+
+button.place(
+    relx=0.5,
+    rely=0.5,
+    anchor = ctk.CENTER
+    )
 
 root.mainloop()

31e498539544d614c4f9b20d363b247f722998b0
Rubin006 <rubinmorina@gmail.com>
2023-04-25 22:03:18 +0100

    Update main.py

--- TK/main.py
+++ TK/main.py
@@ -1,43 +1,150 @@
 import tkinter as tk
 import customtkinter as ctk
+import numpy as np
+import matplotlib.pyplot as plt
 
 ctk.set_appearance_mode("Dark")
 
 ctk.set_default_color_theme("green")
 
-root = ctk.CTk()
-root.geometry("800x600")
-root.title("Shape thing")
 
-
-def shape(choice):
-    sides = []
-    if choice == "Triangle":
-
-        l1 = ctk.CTkLabel(master=root, text="Side 1")
-        l1.grid(padx=40)
-        s1 = ctk.CTkEntry(master=root)
-        s1.grid(padx=40,pady=40)
-
-    elif choice == "Rectangle":
-        print(4)
-    elif choice == "Pentagon":
-        print(5)
+class Frame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master,**kwargs)
 
 
 
+class App(ctk.CTk):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.geometry("800x600")
+        self.title("Shape Thing")
+        self.grid_rowconfigure(5, weight=1)
+        self.grid_columnconfigure(5, weight=1)
 
-shapes = ctk.CTkOptionMenu(
-    master=root,
-    fg_color="#E80",
-    values=["Triangle","Rectangle","Pentagon"],
-    command=shape
-)
-shapes.place(
-    relx=0.05,
-    rely=0.05,
-    anchor=ctk.NW
-)
+        self.sides = Frame(
+            master=self,
+            width=200,
+            height=550,
+            border_width=3,
+            fg_color="#85F"
+        )
 
-root.mainloop()
+        self.sides.grid(
+            row=0,
+            column=0,
+            padx=20,
+            pady=20,
+            sticky="nsew"
+        )
+
+
+        self.side1 = ctk.CTkEntry(
+            master=self.sides,
+            placeholder_text="First Side",
+            width=200,
+            height=50,
+            border_width=2,
+            corner_radius=10
+        )
+        self.side1.pack(
+            padx=25,
+            pady=25
+        )
+
+        self.side2 = ctk.CTkEntry(
+            master=self.sides,
+            placeholder_text="Second Side",
+            width=200,
+            height=50,
+            border_width=2,
+            corner_radius=10
+        )
+        self.side2.pack(
+            padx=25,
+            pady=25
+        )
+
+        self.side3 = ctk.CTkEntry(
+            master=self.sides,
+            placeholder_text="Third Side",
+            width=200,
+            height=50,
+            border_width=2,
+            corner_radius=10
+        )
+        self.side3.pack(
+            padx=25,
+            pady=25
+        )
+
+        
+
+        self.button = ctk.CTkButton(
+            master=self.sides,
+            text="Calculate if Triangle is Possible",
+            width=200,
+            height=50,
+            border_width=3,
+            fg_color="#85F",
+            border_color="black",
+            command= lambda: self.traingle_inequality(self.side1,self.side2,self.side3)
+        )
+        self.button.pack(
+            padx=25,
+            pady=25
+        )
+
+        self.txt = ctk.CTkTextbox(
+            master=self.sides,
+            width=200,
+            height=50
+        )
+
+        self.txt.pack(
+            padx=25,
+            pady=25
+        )
+        
+    def traingle_inequality(self,side1,side2,side3):
+        try:
+            sides= sorted([int(side1.get()),int(side2.get()),int(side3.get())])
+            a = sides[0]
+            b = sides[1]
+            c = sides[2]
+            self.txt.delete(1.0, ctk.END)
+            if a + b > c:
+                self.txt.insert(1.0,"Triangle is possible")
+                
+                angle1 = np.arccos(((a**2)+(c**2)-(b**2))/(2*a*c))
+                angle2 = np.arccos(((b**2)+(c**2)-(a**2))/(2*b*c))
+                gradient1 = np.tan(angle1)
+                gradient2 = -np.tan(angle2)
+
+                x1 = np.arange(0,c,0.01)
+                y1 = [0 for i in range(len(x1))]
+                x2 = np.arange(0,a,0.01)
+                y2 = gradient1*x2
+                if b != c:
+                    x3 = np.arange(b,c,0.01)
+                    y3 = gradient2*x3
+                else:
+                    y3 = np.arange(0,gradient2,0.01)
+                    x3 = [b for i in range(len(y3))]
+                
+                plt.plot(x1,y1)
+                plt.plot(x2,y2)
+                plt.plot(x3,y3)
+                plt.show()
+            else:
+                self.txt.insert(1.0,"Triangle is not possible")
+
+        except ValueError:
+            print("Please enter 3 valid sides")
+    
+
+
+if __name__ == "__main__":
+    root = App()
+    root.mainloop()
 

7fb3be8b382b1d8678eb18c33064013e23290318
Rubin-006 <122726254+Rubin-006@users.noreply.github.com>
2023-04-25 12:09:34 +0100

    Update main.py

--- TK/main.py
+++ TK/main.py
@@ -5,24 +5,39 @@ ctk.set_appearance_mode("Dark")
 
 ctk.set_default_color_theme("green")
 
-
 root = ctk.CTk()
 root.geometry("800x600")
-root.title("Fun Testing")
+root.title("Shape thing")
 
-        
-button = ctk.CTkButton(
-    master = root,
-    text = "Press Me",
-    command= lambda: print("Don't Press me"),
+
+def shape(choice):
+    sides = []
+    if choice == "Triangle":
+
+        l1 = ctk.CTkLabel(master=root, text="Side 1")
+        l1.grid(padx=40)
+        s1 = ctk.CTkEntry(master=root)
+        s1.grid(padx=40,pady=40)
+
+    elif choice == "Rectangle":
+        print(4)
+    elif choice == "Pentagon":
+        print(5)
+
+
+
+
+shapes = ctk.CTkOptionMenu(
+    master=root,
     fg_color="#E80",
-    hover_color="#E50"
-    )
-
-button.place(
-    relx=0.5,
-    rely=0.5,
-    anchor = ctk.CENTER
-    )
+    values=["Triangle","Rectangle","Pentagon"],
+    command=shape
+)
+shapes.place(
+    relx=0.05,
+    rely=0.05,
+    anchor=ctk.NW
+)
 
 root.mainloop()
+
